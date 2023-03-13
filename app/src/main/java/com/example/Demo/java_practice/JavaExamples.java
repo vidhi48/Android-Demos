8277d1e1ea3f67444028b0df8f23a15e7a64251e
vidhi48 <pvidhi0408@gmail.com>
2023-03-13 18:50:19 +0530

    TE7-T686 Basics of java.

---
+++ app/src/main/java/com/example/Demo/java_practice/JavaExamples.java
@@ -0,0 +1,36 @@
+package com.example.Demo.java_practice;
+
+public class JavaExamples {
+    static int eno;
+    static String name;
+    static void set(int id, String n) {
+        eno = id;
+        name = n;
+    }
+    static void get() {
+        System.out.println("Id: "+ eno);
+        System.out.println("Name: "+ name);
+    }
+
+    public static void main(String[] args) {
+        JavaExamples.set(1, "vidhi");
+        //JavaExamples.get();
+        JavaExamples.get();
+    }
+}
+
+class Demo {
+    int age;
+    String name;
+    Demo(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        Demo d1 = new Demo(20, "vidhi");
+        System.out.println(d1.age + d1.name);
+        //Demo d2 = d1.clone();
+
+    }
+}
