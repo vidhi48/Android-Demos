4767aeab269b35aaa33c8d99f4b84b9d73fac650
vidhi48 <pvidhi0408@gmail.com>
2023-05-30 19:06:16 +0530

    Booking screen completed.

---
+++ app/src/main/java/com/example/demo/ResortScreen.kt
@@ -0,0 +1,18 @@
+package com.example.demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityResortScreenBinding
+
+class ResortScreen : AppCompatActivity() {
+
+    private lateinit var binding: ActivityResortScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityResortScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
