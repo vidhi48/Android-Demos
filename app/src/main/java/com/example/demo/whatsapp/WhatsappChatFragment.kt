202e00032a4a26305748ea37f233b469d63e660f
vidhi48 <pvidhi0408@gmail.com>
2023-07-25 17:08:42 +0530

    TE7-T1046: Notification added in Whatsapp screen.

--- app/src/main/java/com/example/demo/whatsapp/WhatsappChatFragment.kt
+++ app/src/main/java/com/example/demo/whatsapp/WhatsappChatFragment.kt
@@ -1,6 +1,14 @@
 package com.example.demo.whatsapp
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -9,8 +17,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.androidproject.R
 import com.example.androidproject.databinding.FragmentWhatsappChatBinding
 import com.example.demo.whatsapp.adapters.WhatsAppChatAdapter
 import com.example.demo.whatsapp.models.ChatViewModel
@@ -20,6 +32,7 @@ class WhatsappChatFragment : Fragment() {
     private lateinit var binding: FragmentWhatsappChatBinding
     private lateinit var adapter: WhatsAppChatAdapter
     private val viewModel: ChatViewModel by viewModels()
+    private val channelId = "channel"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -75,6 +88,7 @@ class WhatsappChatFragment : Fragment() {
                 binding.message.text.clear()
                 autoResponse()
             }
+            sendNotification()
         }
     }
 
@@ -82,5 +96,47 @@ class WhatsappChatFragment : Fragment() {
         Handler(Looper.getMainLooper()).postDelayed({
             viewModel.receiveMessage("Hello")
         }, 1000)
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelId, name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager =
+                requireContext().getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun sendNotification() {
+        createNotificationChannel()
+        val intent = Intent(requireContext(), WhatsappChatFragment::class.java)
+        val pendingIntent =
+            PendingIntent.getActivity(requireContext(), 0, intent, PendingIntent.FLAG_IMMUTABLE)
+        val bitmap = BitmapFactory.decodeResource(context?.resources, R.drawable.profile_chat4)
+        val notificationBuilder = NotificationCompat.Builder(requireContext(), channelId)
+            .setSmallIcon(R.drawable.baseline_message_24)
+            .setContentTitle(getString(R.string.messageTitle))
+            .setContentText(getString(R.string.messageContent))
+            .setLargeIcon(bitmap)
+            .addAction(R.drawable.baseline_reply_24, getString(R.string.reply),null)
+            .addAction(R.drawable.baseline_archive_24,getString(R.string.archive), null)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        with(context?.let { NotificationManagerCompat.from(it) }) {
+            if (ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return
+            }
+            this?.notify(1, notificationBuilder.build())
+        }
     }
 }
